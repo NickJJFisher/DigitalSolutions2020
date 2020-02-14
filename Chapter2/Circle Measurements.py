3edbb4bee641631fc7036980f9339100daf83abc
Nicholas Fisher <s05913@sheldoncollege.com>
2020-02-14 12:04:50 +1000

    14/2/20 #2

---
+++ Chapter2/Circle Measurements.py
@@ -0,0 +1,6 @@
+import math
+
+r = float(input("Enter the radius of your circle: "))
+c = (2 * math.pi * r)
+a = (math.pi * r ** 2)
+print("For a circle with the radius of ", r, "The area is:" , a ,"units^2, and the circumference is", c , "units.")
