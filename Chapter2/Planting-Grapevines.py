8ac3adbad0e077618fcb653aa9e54cc00c092bea
Nicholas Fisher <s05913@sheldoncollege.com>
2020-02-14 12:20:51 +1000

    14/2/20 #2

---
+++ Chapter2/Planting-Grapevines.py
@@ -0,0 +1,8 @@
+r = int(input ("Enter the Length of the rows in feet: "))
+e = int(input ("Enter the amount of space used by an end-post assembly: "))
+s = int(input ("Enter the space between vines: "))
+
+v = ((r - 2 * e)/s)
+v1 = v // 1
+
+print ("The number of grapevines that will fit in the row is", v1 )
