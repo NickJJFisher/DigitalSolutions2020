f2744ae3caa4e764cfc2f2e5d8c2085594bdd3d0
Nicholas Fisher <s05913@sheldoncollege.com>
2020-02-19 13:29:06 +1000

    19/2/20

---
+++ Homework/Double Triangle.py
@@ -0,0 +1,19 @@
+import turtle
+turtle.fillcolor('white')
+turtle.begin_fill()
+turtle.showturtle()
+turtle.speed(50)
+turtle.forward(50)
+turtle.right(240)
+turtle.forward(50)
+turtle.right(240)
+turtle.forward(50)
+turtle.end_fill()
+
+#2nd Triangle
+turtle.right(305)
+turtle.right(220)
+turtle.forward(100)
+turtle.right(150)
+turtle.forward(100)
+turtle.done()
