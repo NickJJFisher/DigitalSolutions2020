dff105c9d0bc84a3c2741765a89dc3bf48a30ba0
Nicholas Fisher <s05913@sheldoncollege.com>
2020-04-02 11:49:16 +1000

    27/03/2020

---
+++ Chapter4/Bug Collector.py
@@ -0,0 +1,7 @@
+bpd = int(input("How many bugs are collected per day? "))
+x = 1
+while x < 5:
+    x = x + 1
+    bugs = x * bpd
+
+print (bugs)
--- Classwork/myGUI.py
+++ Classwork/myGUI.py
@@ -1,9 +1,15 @@
-from guizero import App, Text, Picture, PushButton, MenuBar
-
+from guizero import App, Text, Picture, PushButton, MenuBar, Slider, TextBox
+t = False
 app = App()
 text = Text(app, align="bottom", text="It's the Kungflu", color="#A3125C", font="Calibri", size="56")
 text1 = Text(app, align="top", text="Keep Schools Open", color="#ABC123", font="Ariel", size="39")
 
+def slider_changed(slider_value):
+    textbox.value = slider_value
+
+
+slider = Slider(app, command=slider_changed)
+textbox = TextBox(app)
 
 def Blue():
     button.bg = "blue"
@@ -36,7 +42,6 @@ menubar = MenuBar(app,
                   ])
 
 button = PushButton(app, image="dt.png", command=Blue)
-
 button.bg = "red"
 
 app.display()
