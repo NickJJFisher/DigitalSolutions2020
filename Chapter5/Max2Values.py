69073b29fbf794183772efce257c4001f461392f
Nicholas Fisher <s05913@sheldoncollege.com>
2020-04-24 09:04:48 +1000

    24/4/20

---
+++ Chapter5/Max2Values.py
@@ -0,0 +1,8 @@
+first = int(input("Enter Number"))
+second = int(input("Enter Number"))
+if first > second:
+    print ("First number is larger")
+if second > first:
+    print("Second number is larger")
+else:
+    print ("They are the same")
