bc8bbd51e6294d7b1c2e57e0114cae16899bacd3
Nicholas Fisher <s05913@sheldoncollege.com>
2020-05-01 12:33:45 +1000

    27/4/20

--- Classwork/plotty.py
+++ Classwork/plotty.py
@@ -1,15 +1,12 @@
 import random
 from guizero import App, Picture, PushButton
 app = App()
+picture = Picture (app, image="PlayingCards/1.png")
 def card():
     mylist = list(range(1,53)) #create list of 52 integars
     random.shuffle(mylist) #randomly shuffer
-    cardNum = mylist[0]
-    randCard = "PlayingCards/" + str(cardNum) + ".png"
-    picture = Picture(app, image=randCard)
+    cardNum = mylist[0] #define card Number to the first on the list
+    randCard = "PlayingCards/" + str(cardNum) + ".png" #define rankcard as file directory of a card
+    picture = Picture(app, image=randCard) #define the picture
 button = PushButton(app, command=card)
-
 app.display()
-
-
-#Using this string
